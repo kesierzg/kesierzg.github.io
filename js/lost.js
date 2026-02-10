f23d7677ee2f6f8c6c0ebd5f5f6dd9ead596e146
kesierzg <gerg64@icloud.com>
2026-02-10 12:04:25 +0100

    minor egg fix

--- js/lost.js
+++ js/lost.js
@@ -1,13 +1,17 @@
 var started = false;
+var audio = document.getElementById("audio");
 
 function start() {
   if (started) return;
   started = true;
 
+  audio.play().catch(function(){});
+}
+
+audio.addEventListener("playing", function () {
   document.getElementById("content").style.display = "block";
   document.body.className = "revealed";
-  document.getElementById("audio").play();
-}
+});
 
 document.addEventListener("click", start);
 document.addEventListener("keydown", start);
